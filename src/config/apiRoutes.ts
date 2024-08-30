aa3f83c737b1ed31ea40f79d1036b52f588c67d7
aldoapicella <aldoapicella@outlook.com>
2024-08-30 02:30:44 -0500

    chore: Add API route for subscription

---
+++ src/config/apiRoutes.ts
@@ -0,0 +1,11 @@
+/**
+ * API_ROUTES object containing the available API routes.
+ */
+export const API_ROUTES = {
+    /**
+     * Subscribe route.
+     *
+     * @method POST
+     */
+    SUBSCRIBE: '/subscribe',
+  };
